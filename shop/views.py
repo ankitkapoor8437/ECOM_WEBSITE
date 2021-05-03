60d6a5fa6a5ff642ac832b4ca9321fbd498d7c16
Ankit Kapoor <ankitkapoor8437@gmail.com>
2021-05-03 22:53:31 +0530

    third

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Contact
 from math import ceil
 
 # Create your views here.
@@ -30,7 +30,15 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return render(request, 'shop/contact.html')
+    if request.method=="POST":
+        print(request)
+        name=request.POST.get('name', '')
+        email=request.POST.get('email', '')
+        phone=request.POST.get('phone', '')
+        desc=request.POST.get('desc', '')
+        contact = Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+    return render(request, "shop/contact.html")
 
 def tracker(request):
     return render(request, 'shop/tracker.html')
