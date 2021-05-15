099653c9ab5821a27cd035f83f820e731d8eb4e0
Ankit Kapoor <ankitkapoor8437@gmail.com>
2021-05-15 18:07:11 +0530

    ROLL BACKED

--- aac/settings.py
+++ aac/settings.py
@@ -119,20 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 import os
+
 STATIC_URL = '/static/'
-
-if DEBUG:
-    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_URL = '/media/'
-
+#  MANAGING MEDIA
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# STATIC_URL = '/static/'
-# #  MANAGING MEDIA
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-# MEDIA_URL ='/media/'
+MEDIA_URL ='/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
